9f68d9ecd0f0aeed507ac633e22f6731e65026ee
fanx <faridazhari46@gmail.com>
2025-10-07 10:47:44 +0700

    fix Display Button error

--- src/components/ui/home/DisplayButton.tsx
+++ src/components/ui/home/DisplayButton.tsx
@@ -8,9 +8,12 @@ type DisplayButtonProps = {
 
 const DisplayButton: React.FC<DisplayButtonProps> = ({ to, children }) => {
   return (
-    <button className="flex justify-center items-center bg-white rounded-full w-[144px] h-[144px] text-black font-bellefair hover:outline-[88px] hover:outline-white/10 md:w-[17rem] md:h-[17rem] lg:w-[17rem] lg:h-[17rem] md:text-[2rem]">
-      <NavLink to={to}>{children}</NavLink>
-    </button>
+    <NavLink
+      className="flex justify-center items-center bg-white rounded-full w-[144px] h-[144px] text-black font-bellefair hover:outline-[88px] hover:outline-white/10 md:w-[17rem] md:h-[17rem] lg:w-[17rem] lg:h-[17rem] md:text-[2rem]"
+      to={to}
+    >
+      {children}
+    </NavLink>
   );
 };
 
